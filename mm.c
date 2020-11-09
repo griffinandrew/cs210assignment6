fa90bfae7b9f4f51cb82814d390aaba3aec45392
Griffin Heyrich <gheyrich@csa1.bu.edu>
2020-11-09 18:10:02 -0500

    adding macros, init and extend from text/class

--- mm.c
+++ mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* bu username : eg. jappavoo */
-    "",
+    "gheyrich",
     /* full name : eg. jonathan appavoo */
-    "",
+    "Griffin Heyrich",
     /* email address : jappavoo@bu.edu */
-    "",
+    "gheyrich@bu.edu",
     "",
     ""
 };
@@ -42,13 +42,72 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+//basic macros and constants
+#define WSIZE 4 //word for header and footer size in bytes
+#define DSIZE 8 //double word size for alignment
+#define CHUNKSIZE (1<<12) //extend heap by this amount
+
+#define MAX(x,y) ((x) > (y) ? (x): (y)) 
+
+#define PACK(size, alloc) ((size)|(alloc)) //pack size and allocates bit into a word 
+
+//read and write a word at address p
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) ((*unsigned int *)(p) = val)
+
+//read size and allocated fields from address p
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+//given block ptr bp compute address of its header and footer
+#define HDRP(bp) ((char*)(bp) - WSIZE)
+#define FTRP(bp) ((char*)(bp) + GET_SIZE(HDRP(bp) - DSIZE)
+
+//given block ptr bp, compute address of next and previous blocks
+#define NEXT_BLKP(bp) ((*char)(bp) + GET_SIZE(HDRP))
+#define PREV_BLKP(bp) ((*char)(bp) + GET_SIZE(((chat *)(bp)- DSIZE)))
+
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    //create the intial empty heap 
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *) -1)
+        return -1;
+    PUT(heap_listp,0); //alignment padding
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE,1)); //prologue header
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE,1)); //prologue footer
+    PUT(heap_listp + (3*WSIZE), PACK(0,1)); //epilogue header
+    heap_listp += (2*WSIZE);
+
+    //extend the heap with free block of CHUNCKSIZE bytes
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
     return 0;
 }
+
+
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+
+//alloc an even number of words to maintain alignment
+    size = (words % 2) ? (words + 1) * WSIZE : words *WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    
+    //initialize free block header anmd footer and the epilogue header
+    PUT(HDRP(bp), PACK(size,0)); //free block header
+    PUT(FTRP(bp), PACK(size,0)); //free block footer
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); //new epilogue header
+
+    return coalesce(bp); //coalesce if the prev block was free 
+}
+
+
+
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
